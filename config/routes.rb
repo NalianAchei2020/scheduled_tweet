292044f284bb588482c3502593c0517bb77c07fe
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-11-08 09:09:01 +0100

    add route for sign_up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # get about
   get 'about', to: 'about#index'
+  get 'signUp', to: 'registration#new'
+post 'signUp', to: 'registration#create'
   root to: 'main#index'
 end
